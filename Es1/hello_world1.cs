e5cb689c3e0f7a9ef6b527f9337c32936e84bafc
Teresa Lamorte <teresa.lamorte@studenti.unimi.it>
2022-03-05 19:09:46 +0100

    Hello + name

---
+++ Es1/hello_world1.cs
@@ -0,0 +1,14 @@
+// See https://aka.ms/new-console-template for more information
+
+//Console.WriteLine("Hello, World!");
+using System;
+namespace ConsoleApp1{
+    static class Program {  
+        // Main function, execution entry point of the program  
+        static void Main(string[] args) // string type parameters  
+        {  
+            // Command line arguments  
+            Console.WriteLine("Hello " + args);
+        }  
+    }
+}
